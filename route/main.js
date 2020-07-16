31838d4b051d6ee5180eb19fc60a41d41a3f477c
Hayato Ishida <hayacon13@gmail.com>
2020-07-16 10:17:52 +0100

    route main js

--- route/main.js
+++ route/main.js
@@ -10,8 +10,16 @@ module.exports = function(app) {
  app.get("/addfood", function(req, res) {
    res.render("addfood.ejs");
  });
- 
-  app.post("/foodadded", function(req,res){
-     res.send(req.body)
-   });
+
+   app.post("/foodadded", function(req,res){
+  let sqlquery = "INSERT INTO food (name, unit, calorie, carbs, fat, protein, salt, sugar) VALUES (?,?,?,?,?,?,?,?)";
+  let newrecord = [req.body.name, req.body.unit, req.body.calorie, req.body.carbs, req.body.fat, req.body.protein, req.body.salt, req.body.sugar];
+  db.query(sqlquery, newrecord, (err, result) => {
+    if(err){
+       return console.log(err.message);
+     }else{
+       res.send("Food is added to database, food : " + req.body.name + "\n unit : " + req.body.unit + "\n calorie : " + req.body.calorie + "\n carbs : " + req.body.carbs + "\n fat : " + req.body.fat + "\n protein : " + req.body.protein + "\n salt : " + req.body.salt + "\n sugar : " + req.body.sugar);
+     }
+  });
+});
 }
