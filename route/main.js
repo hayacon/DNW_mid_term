320f454cf268820ce6ce5c4700a8972650d1c83c
Hayato Ishida <hayacon13@gmail.com>
2020-07-22 10:13:25 +0100

    route

--- route/main.js
+++ route/main.js
@@ -18,32 +18,34 @@ module.exports = function(app) {
   /* This interaction is to add new set of data into the database.
   It takes 8 items from user as a input, and add them to the apropriate location on database
   */
-  let sqlquery = "INSERT INTO food (name, unit, calorie, carbs, fat, protein, salt, sugar) VALUES (?,?,?,?,?,?,?,?)";
-  let newrecord = [req.body.name, req.body.unit, req.body.calorie, req.body.carbs, req.body.fat, req.body.protein, req.body.salt, req.body.sugar];
-  db.query(sqlquery, newrecord, (err, result) => {
-    if(err){
-       return console.log(err.message);
-     }else{
-       res.send('<p> Food is added to database </p>' + '<li> Food : ' + req.body.name + '</li>' + '<li> Unit : ' + req.body.unit + '</li>' + '<li> Calorie : ' + req.body.calorie + '</li>' + '<li> Carbs : ' + req.body.carbs + '</li>' + '<li> Fat : ' + req.body.fat + '</li>' + '<li> Protein : ' + req.body.protein + '</li>'+ '<li> Salt : ' + req.body.salt + '</li>'+ '<li>Sugar : ' + req.body.sugar + '</li>'+ '<a href="/addfood"> Back </a>' +
-       '<ul class="nav navbar-nav"><li><a href="/">Home</a></li><li><a href="/about">About</a></li><li><a href="/addfood">Add Food</a></li><li><a href="/search">Search Food</a></li><li><a href="/list">Foods List</a></li></ul>')
-     }
-   });
+    let sqlquery = "INSERT INTO food (name, unit, calorie, carbs, fat, protein, salt, sugar) VALUES (?,?,?,?,?,?,?,?)";
+    let newrecord = [req.body.name, req.body.unit, req.body.calorie, req.body.carbs, req.body.fat, req.body.protein, req.body.salt, req.body.sugar];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+      if(err){
+         return console.log(err.message);
+       }else{
+         res.send('<p> Food is added to database </p>' + '<li> Food : ' + req.body.name + '</li>' + '<li> Unit : ' + req.body.unit + '</li>' + '<li> Calorie : ' + req.body.calorie + '</li>' + '<li> Carbs : ' + req.body.carbs + '</li>' + '<li> Fat : ' + req.body.fat + '</li>' + '<li> Protein : ' + req.body.protein + '</li>'+ '<li> Salt : ' + req.body.salt + '</li>'+ '<li>Sugar : ' + req.body.sugar + '</li>'+ '<a href="/addfood"> Back </a>' +
+         '<ul class="nav navbar-nav"><li><a href="/">Home</a></li><li><a href="/about">About</a></li><li><a href="/addfood">Add Food</a></li><li><a href="/search">Search Food</a></li><li><a href="/list">Foods List</a></li></ul>')
+       }
+     });
   });
 
 /* This route is for user to update existing data on the database. Input is values from a form on search-result.ejs : id, unit, calorie, carbs, fat, protein, salt, sugar, and output is result of process.
 */
   app.post("/update-food", function(req,res){
   /*This interaction is for user to update existing data on the database. Input is id, unit, calorie, carbs, fat, protein, salt, sugar).It find apropriate location on the dabase based on given id and update it with input values */
-  let sqlquery = "UPDATE food SET calorie=?, carbs=?, fat=?, protein=?, salt=?, sugar=?, unit=? WHERE id = ?";
-  let newrecord = [req.body.calorie, req.body.carbs, req.body.fat, req.body.protein, req.body.salt, req.body.sugar, req.body.unit, req.body.id];
-  db.query(sqlquery, newrecord, (err, result) => {
-    if(err){
-       return console.log(err.message);
-     }else{
-       res.send('<p> Food is now updated on database </p>' + '<li> Unit : ' + req.body.unit + '</li>' + '<li> Calorie : ' + req.body.calorie + '</li>' + '<li> Carbs : ' + req.body.carbs + '</li>' + '<li> Fat : ' + req.body.fat + '</li>' + '<li> Protein : ' + req.body.protein + '</li>'+ '<li> Salt : ' + req.body.salt + '</li>'+ '<li>Sugar : ' + req.body.sugar + '</li>'+ '<a href="/search"> Back </a>' +
-       '<ul class="nav navbar-nav"><li><a href="/">Home</a></li><li><a href="/about">About</a></li><li><a href="/addfood">Add Food</a></li><li><a href="/search">Search Food</a></li><li><a href="/list">Foods List</a></li></ul>')
-     }
-   });
+    let sqlquery = "UPDATE food SET calorie=?, carbs=?, fat=?, protein=?, salt=?, sugar=?, unit=? WHERE id = ?";
+    let newrecord = [req.body.calorie, req.body.carbs, req.body.fat, req.body.protein, req.body.salt, req.body.sugar, req.body.unit, req.body.id];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+      if(err){
+         return console.log(err.message);
+       }else{
+         res.send('<p> Food is now updated on database </p>' + '<li> Unit : ' + req.body.unit + '</li>' + '<li> Calorie : ' + req.body.calorie + '</li>' + '<li> Carbs : ' + req.body.carbs + '</li>' + '<li> Fat : ' + req.body.fat + '</li>' + '<li> Protein : ' + req.body.protein + '</li>'+ '<li> Salt : ' + req.body.salt + '</li>'+ '<li>Sugar : ' + req.body.sugar + '</li>'+ '<a href="/search"> Back </a>' +
+         '<ul class="nav navbar-nav"><li><a href="/">Home</a></li><li><a href="/about">About</a></li><li><a href="/addfood">Add Food</a></li><li><a href="/search">Search Food</a></li><li><a href="/list">Foods List</a></li></ul>')
+       }
+     });
   });
 
 /* This route is for deleting a data from the database.
@@ -53,6 +55,7 @@ This route takes id as a input and output the result of deleting process. */
     This interaction only need id as a input, and it deletes all values/items with that id*/
       let sqlquery = "DELETE FROM food WHERE id=?";
       let id = [req.body.id];
+
       db.query(sqlquery, id, (err, result) => {
         if(err){
           return console.log(err.message);
@@ -70,27 +73,30 @@ This route takes id as a input and output the result of deleting process. */
 // Route to search data on the database.Input is a keyword from user on searching form on search.ejs and output search result.
   app.get("/search-result", function(req, res) {
     /*This interaction for select item that matches user's input from the database. Input is keyword from user and output is set of values if there is any match. */
-    let sqlquery = "SELECT*FROM food WHERE name like ?";
-    let word = [req.query.keyword];
+      let sqlquery = "SELECT*FROM food WHERE name like ?";
+      let word = [req.query.keyword];
 
-    db.query(sqlquery, word, (err, result) => {
-    if(err)
-    {
-      res.render("search-result.ejs")
-    }else{
-      res.render("search-result.ejs", {foodresult:result});
-    }
-    });
+      db.query(sqlquery, word, (err, result) => {
+      if(err)
+      {
+        res.render("search-result.ejs")
+      }else{
+        res.render("search-result.ejs", {foodresult:result});
+      }
+      });
   });
+
 /*This route is for get all existing data from the data. There is not input. Output is all values on the dataset. */
   app.get("/list", function(req, res) {
     /*This interaction simply get all data form the database. With no input, it just output akk data and values.*/
     let sqlquery = "SELECT*FROM food ORDER BY name";
-  db.query(sqlquery, (err, result)=>{
-    if(err) res.render("list.ejs");
-    res.render("list.ejs", {availbleFoods: result});
+    
+    db.query(sqlquery, (err, result)=>{
+      if(err) res.render("list.ejs");
+      res.render("list.ejs", {availbleFoods: result});
+    });
   });
-  });
+
 /*This route is for getting data with certain ids from the database.
 Input is ids, and output is all values that match input id. */
   app.get("/counter", function(req, res) {
@@ -98,6 +104,7 @@ Input is ids, and output is all values that match input id. */
     let sqlquery = "SELECT * FROM food WHERE id IN(?)";
     let foods = [];
     let id = [req.query.id];
+
     db.query(sqlquery, id, (err, result) => {
       if(err){
         return console.log(err.message);
@@ -105,7 +112,5 @@ Input is ids, and output is all values that match input id. */
         res.render("counter.ejs", {checkedFood: result.name});
       }
     })
-
   })
-
 }
